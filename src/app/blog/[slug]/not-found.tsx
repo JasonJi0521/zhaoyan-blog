53d6c90782843d4f813244f30cda3de28ed83e0e
Zhaoyan Ji <zhaoyanji0521@gmail.com>
2025-02-25 14:41:44 -0500

    zhaoyan blog v4: fixed deployment issue

--- src/app/blog/[slug]/not-found.tsx
+++ src/app/blog/[slug]/not-found.tsx
@@ -1,6 +1,3 @@
-import Link from 'next/link'
-import { Button } from '@/components/ui/button'
-
 export default function NotFound() {
     return (
         <div className="min-h-[60vh] flex flex-col items-center justify-center">
